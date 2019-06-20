63c9b3988e705b1c6065fb787cd796d7c906a84c
zhixiang <zhou949613654@gmail.com>
2019-06-20 17:53:59 +0200

    1

--- WebTimeSheetM/src/main/webapp/scripts/delete.js
+++ WebTimeSheetM/src/main/webapp/scripts/delete.js
@@ -14,17 +14,3 @@ $(function(){
 })
 
 
-$(function(){
-	$(".delete").click(function(){
-		var label = $(this).next(":hidden").val();
-		var flag = confirm("sei sicuro di cancellare commessa: " + label + "?");
-		if(flag){
-			var url = $(this).attr("href");
-			
-			$("#_form").attr("action", url);
-			$("#_method").val("DELETE");
-			$("#_form").submit();
-		}
-		return false;
-	});
-})
